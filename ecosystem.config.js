5089931f470106eccaa03df4eda492bcca68eff8
minhnt58 <tienminh.uet@gmail.com>
2017-12-22 17:24:01 +0700

    ok

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name : "story-crawler",
+    script : "./index.js",
+    watch: false,
+    autorestart : false,
+    exec_mode: "folk"
+  }]
+}
--- test.js
+++ test.js
@@ -1,19 +1,21 @@
-let url = 'https://drive.google.com/get_video_info?docid=0B0oRBXo0O_gdc1pReWN0ZlVUcVU';
+// let url = 'https://drive.google.com/get_video_info?docid=0B0oRBXo0O_gdc1pReWN0ZlVUcVU';
+//
+// const request = require('request');
+// const queryString = require('query-string');
+//
+// request(url, function (err, response, body) {
+//   if (err) console.log(err);
+//
+//   // console.log('response', response);
+//   console.log('body', body);
+//   console.log('=========================');
+//
+//   const jsonBody = queryString.parse(body);
+//   console.log('jsonBody', jsonBody);
+// })
+//
+// function extractVideoUrlFromString (stringResponse) {
+//
+// }
 
-const request = require('request');
-const queryString = require('query-string');
-
-request(url, function (err, response, body) {
-  if (err) console.log(err);
-
-  // console.log('response', response);
-  console.log('body', body);
-  console.log('=========================');
-
-  const jsonBody = queryString.parse(body);
-  console.log('jsonBody', jsonBody);
-})
-
-function extractVideoUrlFromString (stringResponse) {
-  
-}
+console.log('ok men');
